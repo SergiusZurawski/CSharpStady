683874be6b8707d876ca0b93d3543b573f9875c4
SergiusZurawski <sergi.zhuravski@gmail.com>
2017-08-29 17:04:04 +0100

    Threads

---
+++ Threads/Programm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using static Threads.SimpleThread;
+
+namespace Tutorial
+{
+    public static class Tutorial
+    {
+       
+        public static void Main()
+         {
+             //Threads.SimpleThread.CallExample();
+             Threads.SimpleThread.CallExampleParametrized();
+             //CallExampleBackGroundProcess
+         }
+
+    }
+}
---
+++ Threads/SimpleThread.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads
+{
+    public static class SimpleThread
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(1000); 
+            }
+        }
+
+        public static void ThreadMethod(object o)
+        {
+            for (int i = 0; i < (int)o; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+        public static void CallExample()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(1000);
+            }
+            t.Join(); // is called on the main Thread; Waits for other threads to finish
+        }
+
+        public static void CallExampleBackGroundProcess()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.IsBackground = true;  // True, application exists immediately
+            //t.IsBackground = false; // false, application exists only if process finishes
+            t.Start();
+        }
+
+        public static void CallExampleParametrized()
+        {
+            Thread t = new Thread(new ParameterizedThreadStart(ThreadMethod));
+            t.Start(5);
+            t.Join();
+        }
+    }
+}
