b466709d91c61e3333ed8ebae132c318937a4b1e
sergii zhuravskyi <szhuravskyi@navinet.net>
2017-09-15 18:04:34 +0100

    Constrains 3

--- Generics/Constrains.cs
+++ Generics/Constrains.cs
@@ -13,7 +13,108 @@ using System.Collections.Generic;
 
 namespace Generics
 {
-    
+    public class Employee
+{
+    private string name;
+    private int id;
+
+    public Employee(string s, int i)
+    {
+        name = s;
+        id = i;
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int ID
+    {
+        get { return id; }
+        set { id = value; }
+    }
+}
+
+    public class GenericListConst<T> where T : Employee
+    {
+        private class Node
+        {
+            private Node next;
+            private T data;
+
+            public Node(T t)
+            {
+                next = null;
+                data = t;
+            }
+
+            public Node Next
+            {
+                get { return next; }
+                set { next = value; }
+            }
+
+            public T Data
+            {
+                get { return data; }
+                set { data = value; }
+            }
+        }
+
+        private Node head;
+
+        public GenericListConst() //constructor
+        {
+            head = null;
+        }
+
+        public void AddHead(T t)
+        {
+            Node n = new Node(t);
+            n.Next = head;
+            head = n;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = head;
+
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        public T FindFirstOccurrence(string s)
+        {
+            Node current = head;
+            T t = null;
+
+            while (current != null)
+            {
+                //The constraint enables access to the Name property.
+                //T are guaranteed to be either an Employee object or an object that inherits from Employee
+                if (current.Data.Name == s)
+                {
+                    t = current.Data;
+                    break;
+                }
+                else
+                {
+                    current = current.Next;
+                }
+            }
+            return t;
+        }
+    }
+    //Multiple constraints can be applied to the same type parameter, and the constraints themselves can be generic types, as follows:
+    class EmployeeList<T> where T : Employee, IEmployee, System.IComparable<T>, new()
+    {
+        // ...
+    }
    
     
 
