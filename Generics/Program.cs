a6741d9f06e6cb55ade3b3c1582f45aac64ddd65
sergii zhuravskyi <szhuravskyi@navinet.net>
2017-10-02 18:58:22 +0100

    Added equal

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Generics;
+using Equality;
 using static Generics.TestGenericList;
 
 namespace Generics
@@ -9,7 +10,8 @@ namespace Generics
         static void Main(string[] args)
         {
             //Generics.GenericListAdvanced<int>.Example2();
-            Generics.EqullityVerification.Example();
+            //Generics.EqullityVerification.Example();
+            Equality.ComparisonsOverridden.Example();
         }
     }
 }
