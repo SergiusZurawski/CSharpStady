7069050229b4b7703738f64c717b9937396665c7
Sergii Zhuravskyi <sergi.zhuravski@gmail.com>
2018-01-02 09:06:38 +0000

    iterators

--- Iterators/Program.cs
+++ Iterators/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Iterators
 {
@@ -11,6 +12,22 @@ namespace Iterators
             foreach (object x in collection)
             {
                 Console.WriteLine(x);
+            }
+
+            // Visualizing an iterator’s workflow
+            IEnumerable<int> iterable = VisualizingAnIteratorWorkflow.CreateEnumerable();
+            IEnumerator<int> iterator = iterable.GetEnumerator();
+            Console.WriteLine("Starting to iterate");
+            while (true) {
+                Console.WriteLine("Calling MoveNext()...");
+                bool result = iterator.MoveNext();
+                Console.WriteLine("... MoveNext result={0}", result);
+                if (!result)
+                {
+                    break;
+                }
+                Console.WriteLine("Fetching Current...");
+                Console.WriteLine("... Current result={0}", iterator.Current);
             }
         }
     }
---
+++ Iterators/VisualizingAnIteratorWorkflow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Iterators
+{
+    // Iterator pattern , with Yeald statment
+    public class VisualizingAnIteratorWorkflow
+    {
+        static readonly string Padding = new string(' ', 30);
+
+        public static IEnumerable<int> CreateEnumerable()
+        {
+            Console.WriteLine("{0}Start of CreateEnumerable()", Padding);
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("{0}About to yield {1}", Padding, i);
+                yield return i;
+                Console.WriteLine("{0}After yield", Padding);
+            }
+            Console.WriteLine("{0}Yielding final value", Padding);
+            yield return -1;
+            Console.WriteLine("{0}End of CreateEnumerable()", Padding);
+
+
+        }
+
+    }
+}
