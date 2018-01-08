03db7aec3cf31ad45cd1547433d5372dd7d1053f
Sergii Zhuravskyi <sergi.zhuravski@gmail.com>
2018-01-08 20:25:47 +0000

    test

--- Iterators/Program.cs
+++ Iterators/Program.cs
@@ -2,6 +2,15 @@
 using System.Collections.Generic;
 
 namespace Iterators
+
+/*
+ * Quirks
+    1. Before MoveNext is called the first time, the Current property will always return the default value for the yield type of the iterator.
+    2. After MoveNext has returned false , the Current property will always return the final value yielded.
+    3. Reset always throws an exception instead of resetting like your manual implementation did. This is required behavior, laid down in the specification.
+    4. The nested class always implements both the generic and nongeneric forms of IEnumerator (and the generic and nongeneric IEnumerable , where appropriate).
+
+*/
 {
     class Program
     {
@@ -29,6 +38,15 @@ namespace Iterators
                 Console.WriteLine("Fetching Current...");
                 Console.WriteLine("... Current result={0}", iterator.Current);
             }
+
+            // Iterator with Break 
+            CallOrder.CallExampleWithBreak();
+            // Iterator with Break And Finially
+            CallOrder.CallExampleWithBreakWithFinially();
+            // Iterator with Break And Finially and Return from Calling method
+            CallOrder.CallExampleWithBreakWithFiniallyAndRetrunInCallingMehod();
+            // Iterator with Break And Finially and manual iteration
+            CallOrder.CallExampleWithBreakWithFiniallyAndRetrunInCallingMehodWithoutForEach();
         }
     }
 }
--- Iterators/VisualizingAnIteratorWorkflow.cs
+++ Iterators/VisualizingAnIteratorWorkflow.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Iterators
 {
@@ -22,8 +23,129 @@ namespace Iterators
             yield return -1;
             Console.WriteLine("{0}End of CreateEnumerable()", Padding);
 
+        }
+    }
+    /*
+        - None of the code in CreateEnumerable is called until the first call to MoveNext .
+        - It’s only when you call MoveNext that any real work gets done. Fetching Current doesn’t run any of your code.
+        - The code stops executing at yield return and picks up again just afterward at the next call to MoveNext .
+        - You can have multiple yield return statements in different places in the method.
+        - The code doesn’t end at the last yield return . Instead, the call to MoveNext that causes you to reach the end of the method is the one that returns false .
+        - Note that methods taking ref or out parameters can’t be implemented with iterator blocks.
+     */
+
+    public class CallOrder
+    {
+
+        public static void CallExample()
+        {
+            Console.WriteLine(CallOrderReturn());
+            /* Output
+                before return
+                finally
+                1
+             */
+        }
+
+        public static int CallOrderReturn()
+        {
+            try
+            {
+                Console.WriteLine("before return");
+                return 1;
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+        }
+
+        //Braking Iterator Early
+        public static void CallExampleWithBreak()
+        {
+            DateTime stop = DateTime.Now.AddSeconds(2);
+            foreach (int i in CountWithTimeLimit(stop))
+            {
+                Console.WriteLine("recieved {0}", i);
+                Thread.Sleep(300);
+            }
+        }
+
+        static IEnumerable<int> CountWithTimeLimit(DateTime limit)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (DateTime.Now >= limit)
+                {
+                    yield break;
+                }
+                yield return i;
+            }
+        }
+
+        //Braking Iterator Early with Finially
+        public static void CallExampleWithBreakWithFinially()
+        {
+            DateTime stop = DateTime.Now.AddSeconds(2);
+            foreach (int i in CountWithTimeLimitWithFinally(stop))
+            {
+                Console.WriteLine("recieved {0}", i);
+                Thread.Sleep(300);
+            }
+        }
+
+        //Braking Iterator Early with Finially And Retrun In Calling Mehod (Code That using the iterator)
+        // It exetures Finally from iterator becasue foreach loop is calling Dispose , dispose is call finally in itterator.
+        public static void CallExampleWithBreakWithFiniallyAndRetrunInCallingMehod()
+        {
+            DateTime stop = DateTime.Now.AddSeconds(2);
+            foreach (int i in CountWithTimeLimitWithFinally(stop))
+            {
+                Console.WriteLine("recieved {0}", i);
+                if (i > 3)
+                {
+                    Console.WriteLine("Returning");
+                    return;
+                }
+                Thread.Sleep(300);
+            }
+        }
+
+        public static void CallExampleWithBreakWithFiniallyAndRetrunInCallingMehodWithoutForEach()
+        {
+            DateTime stop = DateTime.Now.AddSeconds(2);
+            IEnumerable<int> iterable = CountWithTimeLimitWithFinally(stop);
+            IEnumerator<int> iterator = iterable.GetEnumerator();
+
+            iterator.MoveNext();
+            Console.WriteLine("recieved {0}", iterator.Current);
+            Thread.Sleep(3000);
+            iterator.MoveNext();
+            Console.WriteLine("recieved {0}", iterator.Current);
 
         }
 
+        static IEnumerable<int> CountWithTimeLimitWithFinally(DateTime limit)
+        {
+            try
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    if (DateTime.Now >= limit)
+                    {
+                        yield break;
+                    }
+                    yield return i;
+                }
+            }
+            finally
+            {
+                //Executes However the loop ends
+                Console.WriteLine("Stopping!");
+            }
+        }
+
+
     }
+
 }
