7db71aaed457777477a5568279c1b9ea3e97c3cf
Sergii Zhuravskyi <sergi.zhuravski@gmail.com>
2018-01-27 22:34:05 +0000

    Partial Type 1

---
+++ PartialTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PartialTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
