d97d4a7769f747279c60c2a41802913a3cae4acf
Sergii Zhuravskyi <sergi.zhuravski@gmail.com>
2018-01-31 06:14:31 +0000

    static classes 2

--- StaticClasses/Program.cs
+++ StaticClasses/Program.cs
@@ -4,9 +4,35 @@ namespace StaticClasses
 {
     class Program
     {
+
+        /*
+         
+            The key features of a utility class are as follows: 
+                All members are static (except a private constructor). 
+                The class derives directly from object . 
+                Typically there’s no state at all, unless some caching or a singleton is involved. 
+                There are no visible constructors. 
+                The class is sealed if the developer remembers to do so.
+
+        */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Invalid for static classes
+
+        }
+    }
+
+    public sealed class NonStaticStringHelper
+    {
+        private NonStaticStringHelper()
+        {
+        }
+
+        public static string Reverse(string input)
+        {
+            char[] chars = input.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
         }
     }
 }
---
+++ StaticClasses/StringHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StaticClasses
+{
+
+    /*
+        Constrains:
+            It can’t be declared as abstract or sealed , although it’s implicitly both.
+            It can’t specify any implemented interfaces. 
+            It can’t specify a base type. 
+            It can’t include any nonstatic members, including constructors.
+            It can’t include any operators. 
+            It can’t include any protected or protected internal members.
+         
+    */
+    public static class StringHelper  // static modifier on class level, omit constructor
+    {
+        /* Invalid for static classes
+            StringHelper variable = null;
+            StringHelper[] array = null; public void Method1(StringHelper x) { }
+            public StringHelper Method1() { return null; }
+            List<StringHelper> x = new List<StringHelper>();
+        */
+
+        public static string Reverse(string input)
+        {
+            char[] chars = input.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
+        }
+    }
+}
