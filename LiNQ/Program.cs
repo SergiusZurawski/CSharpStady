3ac99f5cb5908bd4c87392edebb20ff6c15e3220
sergii zhuravskyi <sergi.zhuravski@gmail.com>
2018-05-08 08:54:38 +0100

    where

--- LiNQ/Program.cs
+++ LiNQ/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using DataModel;
+using System.Linq;
+using WhereImplementation;
 
 namespace LiNQExample
 {
@@ -7,6 +10,20 @@ namespace LiNQExample
         static void Main(string[] args)
         {
             BrakeDownQuery.Example1();
+            Example1();
+
+            TranslationExample.Example();
+        }
+
+        static void Example1()
+        {
+            // Every query expression starts off with the source of a sequence of data:
+            // from element in source
+            // Projection - select clause - 
+            // select expression
+            var query = from user in SampleData.AllUsers select user;
+
+            foreach (var user in query) { Console.WriteLine(user); }
         }
     }
 }
---
+++ LiNQ/WhereImplementation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WhereImplementation
+{
+    static class Extensions
+    {
+        public static Dummy<T> Where<T>(this Dummy<T> dummy,
+                                        Func<T, bool> predicate)
+        {
+            Console.WriteLine("Where called");
+            return dummy;
+        }
+    }
+
+    class Dummy<T>
+    {
+        public Dummy<U> Select<U>(Func<T, U> selector)
+        {
+            Console.WriteLine("Select called");
+            return new Dummy<U>();
+        }
+    }
+
+    class TranslationExample
+    {
+        public static void Example()
+        {
+            var source = new Dummy<string>();
+
+            var query = from dummy in source
+                        where dummy.ToString() == "Ignored"
+                        select "Anything";
+        }
+
+        public static void ExampleConverted()
+        {
+            var source = new Dummy<string>();
+
+            var query0 = source.Where((Dummy<string> dummy) => dummy.ToString() == "Ignored");
+            var query = source.Where(dummy => dummy.ToString() == "Ignored").Select(dummy => "Anything");
+            var query1 = source.Where(dummy => dummy.ToString() == "Ignored").Select<int>(delegate(String a) { return 1; });
+        }
+    }
+
+}
