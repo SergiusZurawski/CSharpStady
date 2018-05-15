603a10af756a920937c7ab8a2551d74482bc915d
sergii zhuravskyi <sergi.zhuravski@gmail.com>
2018-05-15 09:05:37 +0100

    continuation

--- LiNQ/Continuation.cs
+++ LiNQ/Continuation.cs
@@ -16,6 +16,14 @@ namespace WhereImplementation
         first-query into identifier second-query-body
         from identifier in ( first-query ) second-query-body
 
+
+        !IMPRORTANT: 
+        contextual keyword "into" doesn't always mean "continuation".
+        When joins—the join ... into clause (which is used for group joins) doesn’t form a continuation. 
+                The important difference is that with a group join, 
+                all the earlier range variables 
+                (apart from the one used to name the right side of the join) can still be used.
+
      */
     public class Continuation
     {
@@ -35,15 +43,36 @@ namespace WhereImplementation
 
             //Translate into this 
             query = from grouped in (from defect in SampleData.AllDefects
-                                            where defect.AssignedTo != null
-                                            group defect by defect.AssignedTo)
-                    select new { Assignee = grouped.Key, Count = grouped.Count() }
+                                     where defect.AssignedTo != null
+                                     group defect by defect.AssignedTo)
+                    select new { Assignee = grouped.Key, Count = grouped.Count() };
+
+            //Translation Second part
+            query = SampleData.AllDefects.Where(defect => defect.AssignedTo != null)
+                                    .GroupBy(defect => defect.AssignedTo)
+                                    .Select(grouped => new { Assignee = grouped.Key, Count = grouped.Count() });
 
         }
 
         public static void Example1()
         {
-            
+            var query = from defect in SampleData.AllDefects where defect.AssignedTo != null
+                        group defect by defect.AssignedTo into grouped
+                        select new { Assignee = grouped.Key, Count = grouped.Count() } into result
+                        orderby result.Count descending
+                        select result;
+
+            foreach (var entry in query)
+            {
+                Console.WriteLine("{0}: {1}", entry.Assignee.Name, entry.Count);
+            }
+
+            //Translated
+            SampleData.AllDefects.Where(defect => defect.AssignedTo != null)
+                       .GroupBy(defect => defect.AssignedTo)
+                       .Select(grouped => new { Assignee = grouped.Key, Count = grouped.Count() })
+                       .OrderByDescending(result => result.Count);  
+                        //The final select clause effectively does nothing, so the C# compiler ignores it.
         }
 
     }
