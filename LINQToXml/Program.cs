3972b1fd11a4b5c7a994eaa3d000bb189556c4f0
Sergii Zhuravskyi <sergi.zhuravski@gmail.com>
2018-05-27 19:18:42 +0100

    chapter 12 XML

---
+++ LINQToXml/Program.cs
@@ -0,0 +1,57 @@
+﻿using DataModel;
+using System;
+using System.Linq;
+using System.Xml.Linq;
+using System.ComponentModel;
+
+namespace LINQToXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example1();
+            Example2();
+            Example3();
+        }
+
+        static void Example1()
+        {
+            var users = new XElement("users",
+               SampleData.AllUsers.Select(user => new XElement("user",
+                       new XAttribute("name", user.Name),
+                       new XAttribute("type", user.UserType)))
+           );
+
+            Console.WriteLine(users);
+
+        }
+
+        static void Example2()
+        {
+            var developers = new XElement("developers",
+               from user in SampleData.AllUsers
+               where user.UserType == UserType.Developer
+               select new XElement("developer", user.Name)
+           );
+            Console.WriteLine(developers);
+
+        }
+
+        static void Example3()
+        {
+            XElement root = XmlSampleData.GetElement();
+
+            var query = root.Element("users").Elements()
+                            .Select(user => new {
+                                Name = (string)user.Attribute("name"),
+                                UserType = (string)user.Attribute("type")
+                            });
+
+            foreach (var user in query)
+            {
+                Console.WriteLine("{0}: {1}", user.Name, user.UserType);
+            }
+        }
+    }
+}
--- LiNQ/SampleData.cs
+++ LiNQ/SampleData.cs
@@ -574,10 +574,33 @@ namespace DataModel
             };
         }
 
+        public static void AssignIDs()
+        {
+            AssignIDs (SampleData.AllProjects,
+                       (project, id) => project.ProjectID = id);
+            AssignIDs (SampleData.AllUsers,
+                       (user, id) => user.UserID = id);
+            AssignIDs (SampleData.AllDefects,
+                       (defect, id) => defect.ID = id);
+            AssignIDs (SampleData.AllSubscriptions,
+                       (subscription, id) => subscription.NotificationSubscriptionID = id);
+        }
+
+        static void AssignIDs<T>(IEnumerable<T> elements,
+                                 Action<T,int> action)
+        {
+            int id = 1;
+            foreach (T element in elements)
+            {
+                action(element, id++);
+            }
+        }
+
         public static DateTime May(int day)
         {
             return new DateTime(2013, 5, day);
         }
+        
     }
 
 }
