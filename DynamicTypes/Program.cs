960d5bdb9320118262e0886f730d7a6550073a42
Sergii Zhuravskyi <sergi.zhuravski@gmail.com>
2018-06-23 20:15:58 +0100

    Dynamic and encapsulation

---
+++ DynamicTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DynamicTypes
+{
+    class Program
+    {
+
+        // The .NET Framework offers two special classes when working with dynamic types: 
+        // DynamicObject and ExpandoObject. 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
