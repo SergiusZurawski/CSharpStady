875e1cb0836d7162e3b3525e97001e60e37bb21b
Sergii Zhuravskyi <sergi.zhuravski@gmail.com>
2018-06-29 22:12:54 +0100

    interfaces

---
+++ Interfaces/ExplicitAndImplicitCall.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class ExplicitAndImplicitCall
+    {
+        public static void Example()
+        {
+            B b = new B();
+            b.CallA();
+
+            C c = new C();
+            //c.CallA();  //Exception
+            ((A)c).CallA();
+            A a = c;
+            a.CallA();
+        }
+    }
+
+    interface A {  void CallA(); }
+
+    class B : A
+    {
+        public void CallA()
+        { Console.WriteLine("A is called"); }
+    }
+
+    class C : A
+    {
+        void A.CallA()
+        { Console.WriteLine("A is called"); }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interfaces
+{
+
+    //interface contains the public 
+    //                       signature of methods, 
+    //                       properties, 
+    //                       events, 
+    //                       and indexers
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
+    interface IExample
+    {
+        string GetResult();
+        int Value { get; set; }
+        event EventHandler ResultRetrieved;
+        int this[string index] { get; set; }
+        //  all interface members are public by defaul
+    }
+    class ExampleImplementation : IExample
+    {
+        public string GetResult()
+        {
+            return "result";
+        }
+        public int Value { get; set; }
+
+        public event EventHandler CalculationPerformed;
+        public event EventHandler ResultRetrieved;
+
+        public int this[string index]
+        {
+            get { return 42; }
+            set { }
+        }
+
+    }
+
+    //Generic interfaces
+    interface IRepository<T>
+    {
+        T FindById(int id);
+        IEnumerable<T> All();
+    }
+
+
+
+
+}
