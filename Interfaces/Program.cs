6e9687ee8736a8a8623a9435ae5578055aa9f766
sergii zhuravskyi <sergi.zhuravski@gmail.com>
2018-06-29 10:17:34 +0100

    Inheritence

---
+++ Interfaces/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    interface IReadOnlyInterface
+    {
+        int Value { get; }
+    }
+    struct ReadAndWriteImplementation : IReadOnlyInterface
+    {
+        public int Value { get; set; }
+    }
+}
