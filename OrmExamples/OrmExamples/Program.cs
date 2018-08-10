4fd9121d1f2aaaa70d3812cdafdb0cb7b01ab394
sergii zhuravskyi <sergi.zhuravski@gmail.com>
2018-08-10 17:44:59 +0100

    TEst

--- OrmExamples/OrmExamples/Program.cs
+++ OrmExamples/OrmExamples/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,4 +15,6 @@ namespace OrmExamples
             Console.ReadKey();
         }
     }
+
+    
 }
